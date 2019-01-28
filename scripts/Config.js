c5838025f7d374092ed76f9c4df6c9f45bebccb3
Matt Rosen <matt@mrosen.com>
2019-01-28 10:59:22 -0500

    Added FAQ content

--- scripts/Config.js
+++ scripts/Config.js
@@ -1,5 +1,6 @@
 Config = {
 	CHAIN_ID: 'ssc-00000000000000000002',
+	RPC_URL: 'https://steemsmartcontracts.tk/',
 	ACCOUNTS_API_URL: 'https://testaccounts.steem-engine.com',
 	NATIVE_TOKEN: 'SSC'
 }
