73e0804a6980cda7d69f4d0bed7a4aad2833d38b
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-02-20 13:57:03 +1000

    chore(app): remove done and bnb

--- scripts/Config-Dev.js
+++ scripts/Config-Dev.js
@@ -1,85 +1,84 @@
 Config = {
-	MAINTENANCE_MODE: false,
-	CHAIN_ID: 'ssc-00000000000000000002',
-	RPC_URL: 'https://testapi.steem-engine.com/',
-	ACCOUNTS_API_URL: 'https://testaccounts.steem-engine.com',
-	CONVERTER_API: 'https://converter-api.steem-engine.com/api',
-	SCOT_API: 'https://scot-api.steem-engine.com/',
-	NODE_API: 'https://node-api.steem-engine.com/v1/',
-	NATIVE_TOKEN: 'SSC',
-	STEEMP_ACCOUNT: 'steemsc',
-	DISABLED_TOKENS: ['BTC', 'LTC', 'STEEM', 'SBD', 'BCC', 'XAP', 'XRP', 'GOLOS', 'DISNEY', 'AMAZON', 'VOICE', 'ETH', 'EOS', 'TIME'],
-	PEGGED_TOKEN: 'STEEMP',
-	PEGGED_TOKENS: [
-		{
-			name: 'Bitcoin',
-			symbol: 'BTC',
-			pegged_token_symbol: 'BTCP'
-		}, 
-		{
-			name: 'Litecoin',
-			symbol: 'LTC',
-			pegged_token_symbol: 'LTCP'
-		},
-		{
-			name: 'EOS',
-			symbol: 'EOS',
-			pegged_token_symbol: 'EOSP'
-		},
-		{
-			name: 'Swift Cash',
-			symbol: 'SWIFT',
-			pegged_token_symbol: 'SWIFTP'
-		}, 
-		{
-			name: 'Weku',
-			symbol: 'WEKU',
-			pegged_token_symbol: 'WEKUP'
-		}, 
-		{
-			name: 'BitShares',
-			symbol: 'BTS',
-			pegged_token_symbol: 'BTSP'
-		},
-		{
-			name: 'BitShares USD',
-			symbol: 'BTSUSD',
-			pegged_token_symbol: 'BTSUSDP'
-		},
-		{
-			name: 'BitShares CNY',
-			symbol: 'BTSCNY',
-			pegged_token_symbol: 'BTSCNYP'
-		},
-		{
-			name: 'BitShares BTC Bridge',
-			symbol: 'BTSBRIDGE.BTC',
-			pegged_token_symbol: 'BRIDGEBTCP'
-		},
-		{
-			name: 'Crypto Peso',
-			symbol: 'PSO',
-			pegged_token_symbol: 'PSOP'
-		},
-		{
-			name: 'ANX',
-			symbol: 'ANX',
-			pegged_token_symbol: 'ANXP'
-		},
-		{
-			name: 'Bitcoin Cash',
-			symbol: 'BCH',
-			pegged_token_symbol: 'BCHP'
-		}, 
-		{
-			name: 'Dogecoin',
-			symbol: 'DOGE',
-			pegged_token_symbol: 'DOGEP'
-		},
-		{
-			name: 'Sand',
-			symbol: 'SAND',
-			pegged_token_symbol: 'EOSSAND'
-		}
-	]
+    MAINTENANCE_MODE: false,
+    CHAIN_ID: 'ssc-00000000000000000002',
+    RPC_URL: 'https://testapi.steem-engine.com/',
+    ACCOUNTS_API_URL: 'https://testaccounts.steem-engine.com',
+    CONVERTER_API: 'https://converter-api.steem-engine.com/api',
+    SCOT_API: 'https://scot-api.steem-engine.com/',
+    NODE_API: 'https://node-api.steem-engine.com/v1/',
+    NATIVE_TOKEN: 'SSC',
+    STEEMP_ACCOUNT: 'steemsc',
+    DISABLED_TOKENS: ['BTC', 'LTC', 'STEEM', 'SBD', 'BCC', 'XAP', 'XRP', 'GOLOS', 'DISNEY', 'AMAZON', 'VOICE', 'ETH', 'EOS', 'TIME', 'DONE', 'BNB'],
+    PEGGED_TOKEN: 'STEEMP',
+    PEGGED_TOKENS: [{
+            name: 'Bitcoin',
+            symbol: 'BTC',
+            pegged_token_symbol: 'BTCP'
+        },
+        {
+            name: 'Litecoin',
+            symbol: 'LTC',
+            pegged_token_symbol: 'LTCP'
+        },
+        {
+            name: 'EOS',
+            symbol: 'EOS',
+            pegged_token_symbol: 'EOSP'
+        },
+        {
+            name: 'Swift Cash',
+            symbol: 'SWIFT',
+            pegged_token_symbol: 'SWIFTP'
+        },
+        {
+            name: 'Weku',
+            symbol: 'WEKU',
+            pegged_token_symbol: 'WEKUP'
+        },
+        {
+            name: 'BitShares',
+            symbol: 'BTS',
+            pegged_token_symbol: 'BTSP'
+        },
+        {
+            name: 'BitShares USD',
+            symbol: 'BTSUSD',
+            pegged_token_symbol: 'BTSUSDP'
+        },
+        {
+            name: 'BitShares CNY',
+            symbol: 'BTSCNY',
+            pegged_token_symbol: 'BTSCNYP'
+        },
+        {
+            name: 'BitShares BTC Bridge',
+            symbol: 'BTSBRIDGE.BTC',
+            pegged_token_symbol: 'BRIDGEBTCP'
+        },
+        {
+            name: 'Crypto Peso',
+            symbol: 'PSO',
+            pegged_token_symbol: 'PSOP'
+        },
+        {
+            name: 'ANX',
+            symbol: 'ANX',
+            pegged_token_symbol: 'ANXP'
+        },
+        {
+            name: 'Bitcoin Cash',
+            symbol: 'BCH',
+            pegged_token_symbol: 'BCHP'
+        },
+        {
+            name: 'Dogecoin',
+            symbol: 'DOGE',
+            pegged_token_symbol: 'DOGEP'
+        },
+        {
+            name: 'Sand',
+            symbol: 'SAND',
+            pegged_token_symbol: 'EOSSAND'
+        }
+    ]
 }
--- scripts/Config-Prod.js
+++ scripts/Config-Prod.js
@@ -1,90 +1,89 @@
 Config = {
-	MAINTENANCE_MODE: false,
-	CHAIN_ID: 'ssc-mainnet1',
-	RPC_URL: 'https://api.steem-engine.com/rpc',
-	ACCOUNTS_API_URL: 'https://api.steem-engine.com/accounts',
-	CONVERTER_API: 'https://converter-api.steem-engine.com/api',
-	SCOT_API: 'https://scot-api.steem-engine.com/',
-	NODE_API: 'https://node-api.steem-engine.com/v1/',
-	NATIVE_TOKEN: 'ENG',
-	STEEMP_ACCOUNT: 'steem-peg',
-	DISABLED_TOKENS: ['BTC', 'LTC', 'STEEM', 'SBD', 'BCC', 'XAP', 'XRP', 'GOLOS', 'DISNEY', 'AMAZON', 'VOICE', 'ETH', 'EOS', 'TIME'],
-	PEGGED_TOKEN: 'STEEMP',
-	PEGGED_TOKENS: [
-		{
-			name: 'Steem',
-			symbol: 'STEEM',
-			pegged_token_symbol: 'STEEMP'
-		}, 
-		{
-			name: 'Bitcoin',
-			symbol: 'BTC',
-			pegged_token_symbol: 'BTCP'
-		}, 
-		{
-			name: 'Litecoin',
-			symbol: 'LTC',
-			pegged_token_symbol: 'LTCP'
-		},
-		{
-			name: 'EOS',
-			symbol: 'EOS',
-			pegged_token_symbol: 'EOSP'
-		}, 
-		{
-			name: 'SWIFT',
-			symbol: 'SWIFT',
-			pegged_token_symbol: 'SWIFTP'
-		}, 
-		{
-			name: 'Weku',
-			symbol: 'WEKU',
-			pegged_token_symbol: 'WEKUP'
-		}, 
-		{
-			name: 'BitShares',
-			symbol: 'BTS',
-			pegged_token_symbol: 'BTSP'
-		},
-		{
-			name: 'BitShares USD',
-			symbol: 'BTSUSD',
-			pegged_token_symbol: 'BTSUSDP'
-		},
-		{
-			name: 'BitShares CNY',
-			symbol: 'BTSCNY',
-			pegged_token_symbol: 'BTSCNYP'
-		},
-		{
-			name: 'BitShares BTC Bridge',
-			symbol: 'BTSBRIDGE.BTC',
-			pegged_token_symbol: 'BRIDGEBTCP'
-		},
-		{
-			name: 'Crypto Peso',
-			symbol: 'PSO',
-			pegged_token_symbol: 'PSOP'
-		},
-		{
-			name: 'ANX',
-			symbol: 'ANX',
-			pegged_token_symbol: 'ANXP'
-		},
-		{
-			name: 'Bitcoin Cash',
-			symbol: 'BCH',
-			pegged_token_symbol: 'BCHP'
-		}, 
-		{
-			name: 'Dogecoin',
-			symbol: 'DOGE',
-			pegged_token_symbol: 'DOGEP'
-		},
-		{
-			name: 'Sand',
-			symbol: 'SAND',
-			pegged_token_symbol: 'EOSSAND'
-		}
-	]
+    MAINTENANCE_MODE: false,
+    CHAIN_ID: 'ssc-mainnet1',
+    RPC_URL: 'https://api.steem-engine.com/rpc',
+    ACCOUNTS_API_URL: 'https://api.steem-engine.com/accounts',
+    CONVERTER_API: 'https://converter-api.steem-engine.com/api',
+    SCOT_API: 'https://scot-api.steem-engine.com/',
+    NODE_API: 'https://node-api.steem-engine.com/v1/',
+    NATIVE_TOKEN: 'ENG',
+    STEEMP_ACCOUNT: 'steem-peg',
+    DISABLED_TOKENS: ['BTC', 'LTC', 'STEEM', 'SBD', 'BCC', 'XAP', 'XRP', 'GOLOS', 'DISNEY', 'AMAZON', 'VOICE', 'ETH', 'EOS', 'TIME', 'DONE', 'BNB'],
+    PEGGED_TOKEN: 'STEEMP',
+    PEGGED_TOKENS: [{
+            name: 'Steem',
+            symbol: 'STEEM',
+            pegged_token_symbol: 'STEEMP'
+        },
+        {
+            name: 'Bitcoin',
+            symbol: 'BTC',
+            pegged_token_symbol: 'BTCP'
+        },
+        {
+            name: 'Litecoin',
+            symbol: 'LTC',
+            pegged_token_symbol: 'LTCP'
+        },
+        {
+            name: 'EOS',
+            symbol: 'EOS',
+            pegged_token_symbol: 'EOSP'
+        },
+        {
+            name: 'SWIFT',
+            symbol: 'SWIFT',
+            pegged_token_symbol: 'SWIFTP'
+        },
+        {
+            name: 'Weku',
+            symbol: 'WEKU',
+            pegged_token_symbol: 'WEKUP'
+        },
+        {
+            name: 'BitShares',
+            symbol: 'BTS',
+            pegged_token_symbol: 'BTSP'
+        },
+        {
+            name: 'BitShares USD',
+            symbol: 'BTSUSD',
+            pegged_token_symbol: 'BTSUSDP'
+        },
+        {
+            name: 'BitShares CNY',
+            symbol: 'BTSCNY',
+            pegged_token_symbol: 'BTSCNYP'
+        },
+        {
+            name: 'BitShares BTC Bridge',
+            symbol: 'BTSBRIDGE.BTC',
+            pegged_token_symbol: 'BRIDGEBTCP'
+        },
+        {
+            name: 'Crypto Peso',
+            symbol: 'PSO',
+            pegged_token_symbol: 'PSOP'
+        },
+        {
+            name: 'ANX',
+            symbol: 'ANX',
+            pegged_token_symbol: 'ANXP'
+        },
+        {
+            name: 'Bitcoin Cash',
+            symbol: 'BCH',
+            pegged_token_symbol: 'BCHP'
+        },
+        {
+            name: 'Dogecoin',
+            symbol: 'DOGE',
+            pegged_token_symbol: 'DOGEP'
+        },
+        {
+            name: 'Sand',
+            symbol: 'SAND',
+            pegged_token_symbol: 'EOSSAND'
+        }
+    ]
 }
