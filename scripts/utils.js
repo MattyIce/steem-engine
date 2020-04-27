34564fa6826a0d1cc71813e190e02ebc6f8c5ff2
lion-200 <i.duz86@outlook.com>
2020-04-27 13:26:19 +0200

    Change steem price endpoint to CoinGecko

--- scripts/utils.js
+++ scripts/utils.js
@@ -72,8 +72,8 @@ function addCommas(nStr, currency) {
 }
 
 function loadSteemPrice(callback) {
-	$.get('https://postpromoter.net/api/prices', function (data) {
-		window.steem_price = parseFloat(data.steem_price);
+	$.get('https://api.coingecko.com/api/v3/simple/price?ids=STEEM&vs_currencies=USD', function (data) {
+		window.steem_price = parseFloat(data.steem.usd);
 
 		if(callback)
 			callback(window.steem_price);
